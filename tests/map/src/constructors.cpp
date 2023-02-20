dbcbc14018b509ad7ad2276512dc201f95a433a9
Toluvia Ruby <truby@pr-d5.kzn.21-school.ru>
2023-02-20 23:03:51 +0300

    map constructors have tested

--- tests/map/src/constructors.cpp
+++ tests/map/src/constructors.cpp
@@ -9,10 +9,8 @@ void map_constructors(void)
     ft::map<std::string, std::string>           my_map_2;
     std::map<std::string, std::string>          std_map_2;
 
-    // std::cout << "here" << std::endl;
     my_map_1[1] = 532;
     std_map_1[1] = 532;
-    // std::cout << my_map_1[1] << std::endl;
     my_map_1[2] = 533;
     std_map_1[2] = 533;
     my_map_1[3] = 534;
@@ -25,7 +23,16 @@ void map_constructors(void)
     std_map_1[6] = 537;
     my_map_1[7] = 538;
     std_map_1[7] = 538;
+    my_map_1[7] = 539;
+    std_map_1[7] = 539;
 
+    my_map_2["Hello"] = "test";
+    std_map_2["Hello"] = "test";
+    my_map_2["How are you"] = "test";
+    std_map_2["How are you"] = "test";
+    my_map_2["Thanks"] = "I'm fine";
+    std_map_2["Thanks"] = "I'm fine";
+    
     {
         test_name("Range constructor test (int)");
         ft::map<int,int>::iterator              my_it_beg = my_map_1.begin();
@@ -48,45 +55,36 @@ void map_constructors(void)
         ft::map<int, int>                       my_map_4(my_map_1);
         std::map<int, int>                      std_map_4(std_map_1);
 
-        std::cout << "my_or: " << my_map_1.size() << " my_copy: " << my_map_4.size() << std::endl;
-        std::cout << "std_or: " << std_map_1.size() << " std_copy: " << std_map_4.size() << std::endl;
-
         assert(my_map_4.size() == my_map_1.size());
         assert(std_map_4.size() == std_map_1.size());
         assert(my_map_4.size() == std_map_4.size());
         assert(my_map_4[1] == std_map_4[1]);
     }
 
-    // my_map_2["Hello"] = "test";
-    // std_map_2["Hello"] = "test";
-    // my_map_2["How are you"] = "test";
-    // std_map_2["How are you"] = "test";
-    // my_map_2["Thanks"] = "I'm fine";
-    // std_map_2["Thanks"] = "I'm fine";
 
-    // {
-    //     test_name("Range constructor test (string)");
-    //     ft::map<std::string,std::string>::iterator              my_it_beg = my_map_2.begin();
-    //     ft::map<std::string,std::string>::iterator              my_it_end = my_map_2.end();
-    //     std::map<std::string,std::string>::iterator             std_it_beg = std_map_2.begin();
-    //     std::map<std::string,std::string>::iterator             std_it_end = std_map_2.end();
+    {
+        test_name("Range constructor test (string)");
+        ft::map<std::string,std::string>::iterator              my_it_beg = my_map_2.begin();
+        ft::map<std::string,std::string>::iterator              my_it_end = my_map_2.end();
+        std::map<std::string,std::string>::iterator             std_it_beg = std_map_2.begin();
+        std::map<std::string,std::string>::iterator             std_it_end = std_map_2.end();
 
-    //     ft::map<std::string,std::string>                      my_map_3(my_it_beg, my_it_end);
-    //     std::map<std::string,std::string>                     std_map_3(std_it_beg, std_it_end);
+        ft::map<std::string,std::string>                      my_map_3(my_it_beg, my_it_end);
+        std::map<std::string,std::string>                     std_map_3(std_it_beg, std_it_end);
 
-    //     assert(my_map_3.size() == std_map_3.size());
+        assert(my_map_3.size() == std_map_3.size());
 
-    //     assert(my_map_3["Hello"] == std_map_3["Hello"]);
-    //     assert(my_map_3["How are you"] == std_map_3["How are you"]);
-    //     assert(my_map_3["Thanks"] == std_map_3["Thanks"]);
-    // }
+        assert(my_map_3["Hello"] == std_map_3["Hello"]);
+        assert(my_map_3["How are you"] == std_map_3["How are you"]);
+        assert(my_map_3["Thanks"] == std_map_3["Thanks"]);
+    }
 
-    // {
-	//     test_name("Copy constructor test (string)");
-    //     ft::map<std::string,std::string>                       my_map_4(my_map_2);
-    //     std::map<std::string,std::string>                     std_map_4(std_map_2);
+    {
+	    test_name("Copy constructor test (string)");
+        ft::map<std::string,std::string>                       my_map_4(my_map_2);
+        std::map<std::string,std::string>                     std_map_4(std_map_2);
 
-    //     assert(my_map_4.size() == std_map_4.size());
-    //     assert(my_map_4["Hello"] == std_map_4["Hello"]);
-    // }
+        assert(my_map_4.size() == std_map_4.size());
+        assert(my_map_4["Hello"] == std_map_4["Hello"]);
+    }
 }
